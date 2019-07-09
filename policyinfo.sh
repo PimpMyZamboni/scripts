719743d3a76835c2775e2a7cb1a289c7079e02c3
PimpMyZamboni <34341165+PimpMyZamboni@users.noreply.github.com>
2019-07-09 10:04:57 -0400

    Create policyinfo.sh

---
+++ policyinfo.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+read -p "Enter master server name : " mastername
+read -p "Enter policy name : " policyname
+echo "Details for policy $policyname are below."
+/usr/openv/netbackup/bin/admincmd/bppllist $policyname -U -M $mastername | egrep -v "Active:|Effective date:|Mult. Data Streams:|Client Encrypt:|Checkpoint:|Policy Priority:|Max Jobs/Policy:|Disaster Recovery:|Collect BMR info:|Volume Pool:|Server Group:|Keyword:|Data Classification:|Residence is Storage Lifecycle Policy:|Application Discovery:|Discovery Lifetime:|ASC Application and attributes:|Granular Restore Info:|Ignore Client Direct:|Use Accelerator:|Client List Type:|Selection List Type:|Application Defined:|PFI Recovery:|Maximum MPX:|Fail on Error:|Number Copies:|Retention Level:|Synthetic:|Checksum Change Detection:|Backup network drvs:|Collect TIR info:"
